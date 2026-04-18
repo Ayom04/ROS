c682c417a4b143c8a2f06f0cc3d66ed0d81f311c
ayom04 <abdullahayomide04@gmail.com>
2026-04-18 21:34:36 +0100

    Update maintainer information and paths for user 'hadizah' in ROS packages

--- src/ros_service_demo/setup.py
+++ src/ros_service_demo/setup.py
@@ -13,8 +13,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ayomide',
-    maintainer_email='ayomide@todo.todo',
+    maintainer='hadizah',
+    maintainer_email='hadizah@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
