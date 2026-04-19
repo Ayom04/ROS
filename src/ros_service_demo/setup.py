67c935e9575568a48ffe93150e2567096090192f
ayom04 <abdullahayomide04@gmail.com>
2026-04-19 08:40:14 +0100

    Update paths and maintainer information to reflect new user context

--- src/ros_service_demo/setup.py
+++ src/ros_service_demo/setup.py
@@ -13,8 +13,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ayomide',
-    maintainer_email='ayomide@todo.todo',
+    maintainer='bytewise',
+    maintainer_email='bytewise@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
